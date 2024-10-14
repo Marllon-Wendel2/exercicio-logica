2270a7cc876741ea3b883fdc6062955c47652b5d
Marllon Home <m.w.p.ferreira@outlook.com>
2024-10-13 21:54:11 -0300

    feat: :memo: add: resolucao 74 em js

--- Lista2/att-74.js
+++ Lista2/att-74.js
@@ -27,10 +27,10 @@
 import cliPromise from "../cliPromise.js"
 
 class Aluno {
-    nome
+    id;
     nota;
-    constructor(nome, nota = 0) {
-        this.nome = nome
+    constructor(id, nota = 0) {
+        this.id = id;
         this.nota = nota;
     }
 }
@@ -38,10 +38,94 @@ class Aluno {
 const testAnswerKey = ["A", "B", "C", "D", "E", "E", "D", "C", "B", "A"];
 const alunos =  [];
 
-
-let perguntaContador = 1;
 let alunoContador = 1;
 
+async function mensagemInicial(){
+    const answer = await cliPromise.question("Deseja criar um novo aluno?: ")
+    interacao(answer);
+}
 
-criaAluno()
+async function interacao(answer) {
+    if(answer.toLowerCase() === 'sim') {
+        const alunoNovo =  new Aluno(alunoContador);
+        const respostaDoAluno = await perguntaRespostas();
+        const nota = await calculaNota(respostaDoAluno);
+        alunoNovo.nota = nota;
+        alunos.push(alunoNovo);
+        console.clear();
+        console.log(`O aluno ${alunoNovo.id} foi registrado com sucesso! Sua nota foi de ${alunoNovo.nota}`);
+        await perguntaSeContinua();
 
+    } else if(answer.toLowerCase() === 'nao') {
+        console.log('Programa encerrado!')
+        cliPromise.close();
+    } else {
+        console.log("Insira um valor valido. (sim ou nao)");
+        mensagemInicial();
+    }
+}
+
+async function perguntaRespostas() {
+    const respostaDoAluno = await cliPromise.question("Informe o gaberito do aluno: ");
+    const listaRespostaDoAluno = respostaDoAluno.toUpperCase().split(',');
+    if(listaRespostaDoAluno.length > 10 || listaRespostaDoAluno.length < 10) {
+        throw new Error("Numero de respostas invalido")
+    } else {
+        return listaRespostaDoAluno
+    }
+}
+
+async function calculaNota(listaDeRespostas) {
+    let nota = 0;
+    for(let resposta = 0; resposta < testAnswerKey.length; resposta++) {
+        if(listaDeRespostas[resposta] === testAnswerKey[resposta]) {
+            nota++
+        }
+    }
+    return nota;
+}
+
+async function perguntaSeContinua() {
+    const continua = await cliPromise.question("Deseja inserir outro aluno? ");
+
+    if(continua.toLowerCase() === 'sim') {
+        alunoContador++;
+        mensagemInicial();
+    } else if(continua.toLowerCase() === 'nao') {
+        exibeResumo();
+        cliPromise.close();
+    } else {
+        console.log("Insira um valor valido. (sim ou nao)")
+    }
+}
+
+async function exibeResumo() {
+    const media = calculaMedia(alunos);
+    const maiorNota = pegarMaiorNota(alunos);
+    const menorNota = pegarMenorNota(alunos);
+
+    console.log(`Foram registrados ${alunoContador} alunos;`);
+    console.log(`A maior nota foi do aluno ${maiorNota.id} com ${maiorNota.nota} pontos!`);
+    console.log(`A menor nota foi do aluno ${menorNota.id} com ${menorNota.nota} pontos!`);
+}
+
+function calculaMedia(lista) {
+    let soma = 0;
+    for(const aluno of alunos) {
+        soma += aluno.nota;
+    }
+
+    return soma/ lista.length;
+}
+
+function pegarMaiorNota(lista) {
+    lista.sort((a, b) => a.nota - b.nota);
+    return lista[lista.length - 1];
+}
+
+function pegarMenorNota(lista) {
+    lista.sort((a, b) => a.nota - b.nota);
+    return lista[0];
+}
+
+mensagemInicial()
--- cliPromise.js
+++ cliPromise.js
@@ -1,6 +1,6 @@
 import readline from 'readline/promises';
 import { stdin as input, stdout as output } from 'process';
 
-const cliPromise = readline.creaInterFace({input, output});
+const cliPromise = readline.createInterface({input, output});
 
 export default cliPromise
